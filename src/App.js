4dc2236246d30e226241a8dc87bfe9ec16f09d9f
IuriVeloso <iuriv.felix@gmail.com>
2019-11-22 22:51:12 -0300

    modified:   package.json 	modified:   src/App.js 	modified:   src/index.js 	modified:   src/services/api.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import {BrowserRouter} from 'react-router-dom';
 
-import Routes from './routes';
-import GlobalStyle from './styles/global';
+import Routes from './routes.js';
+import GlobalStyle from './styles/global.js';
 
 function App() {
   return(
     <>
-    <BrowserRouter>
-        {/*<Header>*/}
-        <Routes/>
-        <GlobalStyle/>
-    </BrowserRouter>
+        <BrowserRouter>
+            <Routes/>
+            <GlobalStyle/>
+        </BrowserRouter>
     </>
   )
 }
