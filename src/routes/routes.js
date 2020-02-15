e70955c052edea314df5d860a462c1fcb757dcaa
Isabella Rovigati <isovigati@gmail.com>
2020-02-15 15:32:16 -0300

    rotas_atualizadas

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -14,6 +14,12 @@ import AddUserPage from '../pages/AddUserPage/index.js';
 import SearchUserPage from '../pages/SearchUserPage/index.js';
 import UserPage from '../pages/UserPage/index.js';
 import Partners from '../pages/PartnersPage/index.js';
+import Schools from '../pages/SchoolsPage/index.js';
+import ONG from '../pages/ONGPage/index.js';
+import Capes from '../pages/CapesPage/index.js';
+import CREAS from '../pages/CREASPage/index.js';
+import CF from '../pages/CFPage/index.js';
+import Capsi from '../pages/CapsiPage/index.js';
 
 export default function Routes() {
     return (
@@ -45,7 +51,13 @@ export default function Routes() {
                     isPrivate
                 />
                 <RouteManager path="/paciente" component={UserPage} isPrivate />
-                <RouteManager path="/parceiros" component={Partners} isPrivate/>}
+                <RouteManager path="/parceiros" component={Partners} isPrivate/>
+                <RouteManager path="/escolas-e-creches" component={Schools} isPrivate/>
+                <RouteManager path="/ONGs" component={ONG} isPrivate/>
+                <RouteManager path="/capes-adulto" component={Capes} isPrivate/>
+                <RouteManager path="/CREAS/CRAS" component={CREAS} isPrivate/>
+                <RouteManager path="/CF/CMS" component={CF} isPrivate/>
+                <RouteManager path="/CAPSI" component={Capsi} isPrivate/>}
             </Switch>
         </ConnectedRouter>
     );
